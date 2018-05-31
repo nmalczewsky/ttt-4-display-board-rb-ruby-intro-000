a56ef5ca2b4b6bf630aac56c8afdd1c9120cebfc
Nicholas Malczewsky <n.malczewsky@gmail.com>
2018-05-31 01:26:30 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -193,7 +193,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[2]).to eq(" O | O | O ")
       expect(rows[3]).to eq("-----------")
       expect(rows[4]).to eq(" O | O | O ")
-      
+
       # *** Comment the line below by adding a # at the line start ***
       # expect(true).to be(true)
     end
